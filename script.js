767245c1bace8a2ee9667f0e57730eface05e4d6
nandonyata <nandonp02@gmail.com>
2022-11-10 19:57:21 +0700

    menambahkan function utk hitung calories

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
+// Awal catur's workspace, menghitung BMI
 const data = {
-  nama: "adit",
-  berat: 120,
-  tinggi: 167,
+  nama: 'adit',
+  berat: 20,
+  tinggi: 175,
+  usia: 21,
+  sex: 'Male',
+  olahraga: 'tidak olahraga',
 };
 
-const classification = [
-  "Under Weight",
-  "normal",
-  "Over Weight",
-  "Obesity (Class I)",
-  "Obesity (Class II)",
-  "Extreme Obesity",
-];
-// let beratBadan = 0
-// let tinggiBadan = 0
+const classification = ['Under Weight', 'normal', 'Over Weight', 'Obesity (Class I)', 'Obesity (Class II)', 'Extreme Obesity'];
 
 function calculateBMI(params) {
   let beratBadan = 0;
@@ -30,8 +25,8 @@ function calculateBMI(params) {
 
 function classificationBMI(data, classification) {
   let bmi = calculateBMI(data);
-  console.log(bmi);
-  let classed = "";
+  //   console.log(bmi);
+  let classed = '';
   if (bmi >= 40) {
     classed = classification[5];
   } else if (bmi >= 35) {
@@ -48,7 +43,45 @@ function classificationBMI(data, classification) {
 
   return classed;
 }
+// Akhir catur's workspace
 
-console.log(classificationBMI(data, classification));
+// Awal nando's workspace, menghitung asupan kalori perhari
+function calculateBMR(data) {
+  let BMR = 0;
+  if (data.sex === 'Male') {
+    let temp = 88.4 + 13.4 * data.berat + 4.8 * data.tinggi - 5.68 * data.usia;
+    BMR = temp;
+  } else {
+    let temp = 447.6 + 9.25 * data.berat + 3.1 * data.tinggi - 4.33 * data.usia;
+    BMR = temp;
+  }
 
-// console.log(calculateBMI(data));
+  return BMR;
+}
+
+function calculateCaloriesNeededPerDay(data) {
+  let bmi = calculateBMI(data);
+  let status = classificationBMI(data, classification);
+  let result = { bmi: bmi, kondisi: status };
+  let ExcerciseLevel = {
+    'tidak olahraga': 1.2,
+    '3 kali seminggu': 1.375,
+    '4 kali seminggu': 1.55,
+    '5 kali seminggu': 1.75,
+    daily: 1.9,
+  };
+
+  let bmr = calculateBMR(data);
+  let temp = bmr * ExcerciseLevel[data.olahraga];
+  result.maintain = temp.toFixed();
+
+  if (status === 'Over Weight' || status === 'Obesity (Class I)' || status === 'Obesity (Class II)' || status === 'Extreme Obesity') {
+    result['fat loss'] = result.maintain - 380;
+  } else if (status === 'Under Weight') {
+    result['gain weight'] = Number(result.maintain) + 380;
+  }
+
+  return result;
+}
+console.log(calculateCaloriesNeededPerDay(data));
+// Akhir nando's workspace
