1777009d28e59fcf3f2bf98f61e5749bb65bb2d3
anttonyh3h3 <muhammadcatur.sp@gmail.com>
2022-11-10 19:59:00 +0700

    add desc

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ const classification = [
   "Obesity (Class II)",
   "Extreme Obesity",
 ];
+
+const desc = [
+  `Waduh kamu under weight nih, kamu kenapa sampe kurang makan gitu ? stress ? atau galau ditinggalin doi ? jangan khawatir nya, kamu bisa mulai hidup sehat dengan makan yang cukup sama jangan kurang tidur ya. Semangat!!`,
+  `Keep up the good work! basil BMI kamu normal, lanjutin perjuangan nya ya. semangat!!`,
+  `Kamu kalo stress makan ya ? kamu Overweight nih, jangan kebanyakan makan ya nanti gendut. Mulai olahraga ya sama kurangin makan tengah malem nya.`,
+  `Ini kamu makan sehari berapa kali sih sampe Obesity (Class I) gini, harus diet ya sama olahraga juga biar berat badan kamu turun.`,
+  `Ini kamu makan sehari berapa kali sih sampe Obesity (Class II) gini, harus diet ya sama olahraga juga biar berat badan kamu turun.`,
+  `Duh ini mah syulit, aku gabisa berkata kata deh. diet ya sama olahraga juga yang rutin.`,
+];
 // let beratBadan = 0
 // let tinggiBadan = 0
 
@@ -28,27 +37,34 @@ function calculateBMI(params) {
   return calculation;
 }
 
-function classificationBMI(data, classification) {
+function classificationBMI(data, classification, desc) {
   let bmi = calculateBMI(data);
   console.log(bmi);
   let classed = "";
+  let descClass = ``;
   if (bmi >= 40) {
     classed = classification[5];
+    descClass = desc[5];
   } else if (bmi >= 35) {
     classed = classification[4];
+    descClass = desc[4];
   } else if (bmi >= 30) {
     classed = classification[3];
+    descClass = desc[3];
   } else if (bmi >= 25) {
     classed = classification[2];
+    descClass = desc[2];
   } else if (bmi >= 18.5) {
     classed = classification[1];
+    descClass = desc[1];
   } else {
     classed = classification[0];
+    descClass = desc[0];
   }
-
+  console.log(descClass);
   return classed;
 }
 
-console.log(classificationBMI(data, classification));
+console.log(classificationBMI(data, classification, desc));
 
 // console.log(calculateBMI(data));
